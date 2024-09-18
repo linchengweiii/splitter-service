e3a7b5705f206714c4d1a28c7e45efefb464414c
linchengweiii <linchengweiii@gmail.com>
2024-09-18 20:20:39 +0800

    feat: add get, patch, and delete

--- pkg/router/expense.go
+++ pkg/router/expense.go
@@ -4,12 +4,16 @@ import (
     "encoding/json"
     "net/http"
 
+    "github.com/gorilla/mux"
     "github.com/linchengweiii/splitter/pkg/group"
     "github.com/linchengweiii/splitter/pkg/expense"
 )
 
 type ExpenseRouter interface {
+    GetExpense(w http.ResponseWriter, r *http.Request)
     PostExpense(w http.ResponseWriter, r *http.Request)
+    PatchExpense(w http.ResponseWriter, r *http.Request)
+    DeleteExpense(w http.ResponseWriter, r *http.Request)
 }
 
 type ExpenseRouterImpl struct {
@@ -18,7 +22,28 @@ type ExpenseRouterImpl struct {
     expenseService *expense.Service
 }
 
-func(router *ExpenseRouterImpl) PostExpense(w http.ResponseWriter, r *http.Request) {
+func (router *ExpenseRouterImpl) GetExpense(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    expenseId := vars["expenseId"]
+
+    expense, err := router.expenseService.Read(expenseId)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    e, err := json.Marshal(expense)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK)
+    w.Write(e)
+}
+
+func (router *ExpenseRouterImpl) PostExpense(w http.ResponseWriter, r *http.Request) {
     var expenseInput expense.Expense
     err := json.NewDecoder(r.Body).Decode(&expenseInput)
     if err != nil {
@@ -56,6 +81,80 @@ func(router *ExpenseRouterImpl) PostExpense(w http.ResponseWriter, r *http.Reque
     w.Header().Set("Content-Type", "application/json")
     w.WriteHeader(http.StatusCreated)
     w.Write(e)
+}
+
+func (router *ExpenseRouterImpl) PatchExpense(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    expenseId := vars["expenseId"]
+
+    var expenseInput expense.Expense
+    err := json.NewDecoder(r.Body).Decode(&expenseInput)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    expenseOutput, err := router.expenseService.Update(expenseId, expenseInput)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    g, err := router.groupService.Read(router.groupId)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    for i, expense := range g.Expenses {
+        if expense.Id == expenseId {
+            g.Expenses[i] = group.Expense{
+                Id: expenseOutput.Id,
+                Description: expenseOutput.Description,
+                Paid: expenseOutput.Paid,
+                Owed: expenseOutput.Owed,
+            }
+            break
+        }
+    }
+
+    e, err := json.Marshal(expenseOutput)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK)
+    w.Write(e)
+}
+
+func (router *ExpenseRouterImpl) DeleteExpense(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    expenseId := vars["expenseId"]
+
+    err := router.expenseService.Delete(expenseId)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    group, err := router.groupService.Read(router.groupId)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    for i, expense := range group.Expenses {
+        if expense.Id == expenseId {
+            group.Expenses = append(group.Expenses[:i], group.Expenses[i+1:]...)
+            break
+        }
+    }
+
+    router.groupService.Update(group)
+
+    w.WriteHeader(http.StatusNoContent)
 }
 
 func NewExpenseRouter(groupId string, groupService *group.Service, expenseService *expense.Service) ExpenseRouter {
--- pkg/router/go.mod
+++ pkg/router/go.mod
@@ -1,6 +1,7 @@
 module github.com/linchengweiii/splitter/pkg/router
 
 require (
+	github.com/gorilla/mux v1.8.1
 	github.com/linchengweiii/splitter/pkg/expense v0.0.0
 	github.com/linchengweiii/splitter/pkg/group v0.0.0
 )
